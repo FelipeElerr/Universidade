68d805dcb2b9c514a4f7b9f6c86f1f5b13664cc6
Felipe Machado <Felipe.Machado@fit-tecnologia.org.br>
2022-10-28 18:15:24 -0300

    Iniciando tela de cadastro de alunos

--- universidade/App.js
+++ universidade/App.js
@@ -4,48 +4,47 @@ import {View, Text, Button} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
-import Ex01 from './Componentes/ex1'
-import Ex02 from './Componentes/ex2'
-import Ex03 from './Componentes/ex3'
-import Ex04 from './Componentes/ex4'
-import Ex05 from './Componentes/ex5'
+import CadastroAluno from './Componentes/cadastroAluno'
+import CadastroDisciplina from './Componentes/cadastroDisciplina'
+import CadastroProfessor from './Componentes/cadastroProfessor'
+import CadastroTurma from './Componentes/cadastroTurma'
+import CadastroHistorico from './Componentes/cadastroHistorico'
 
 const Pilha = createNativeStackNavigator();
 
-function TelaExercicios(props){
+function TelasCadastro(props){
   return(
     <View>
-        <Text>Clique em um dos botões abaixo para ver o exercício desejado</Text>
 
         <Button
                 style = {{backgroundColor:'blue'}}
-                title = "Ex01"
-                onPress = {()=>props.navigation.navigate('Ex01')} 
+                title = "Cadastro de Aluno"
+                onPress = {()=>props.navigation.navigate('CadastroAluno')} 
         />
 
         <Button
                 style = {{backgroundColor:'green'}}
-                title = "Ex02"
-                onPress = {()=>props.navigation.navigate('Ex02')} 
+                title = "Cadastro de Disciplina"
+                onPress = {()=>props.navigation.navigate('CadastroDisciplina')} 
         />
 
         <Button
                 style = {{backgroundColor:'blue'}}
-                title = "Ex03"
-                onPress = {()=>props.navigation.navigate('Ex03')} 
+                title = "Cadastro de Professor"
+                onPress = {()=>props.navigation.navigate('CadastroProfessor')} 
         />
 
         <Button
                 style = {{backgroundColor:'green'}}
-                title = "Ex04"
-                onPress = {()=>props.navigation.navigate('Ex04')} 
+                title = "Cadastro de Turma"
+                onPress = {()=>props.navigation.navigate('CadastroTurma')} 
         />
 
 
         <Button
                 style = {{backgroundColor:'blue'}}
-                title = "Ex05"
-                onPress = {()=>props.navigation.navigate('Ex05')} 
+                title = "Cadastro de Historico"
+                onPress = {()=>props.navigation.navigate('CadastroHistorico')} 
         />
 
 
@@ -59,12 +58,12 @@ export default function App(){
 
         <NavigationContainer independent={true}>
             <Pilha.Navigator>
-                <Pilha.Screen name='TelaExercicios' component={TelaExercicios} options={{title:'Exercícios'}}/>
-                <Pilha.Screen name='Ex01' component={Ex01}/>
-                <Pilha.Screen name='Ex02' component={Ex02}/>
-                <Pilha.Screen name='Ex03' component={Ex03}/>
-                <Pilha.Screen name='Ex04' component={Ex04}/>
-                <Pilha.Screen name='Ex05' component={Ex05}/>
+                <Pilha.Screen name='TelasCadastro' component={TelasCadastro} options={{title:'Opções de Cadastro'}}/>
+                <Pilha.Screen name='CadastroAluno' component={CadastroAluno}/>
+                <Pilha.Screen name='CadastroDisciplina' component={CadastroDisciplina}/>
+                <Pilha.Screen name='CadastroProfessor' component={CadastroProfessor}/>
+                <Pilha.Screen name='CadastroTurma' component={CadastroTurma}/>
+                <Pilha.Screen name='CadastroHistorico' component={CadastroHistorico}/>
             </Pilha.Navigator>
         </NavigationContainer>
         
---
+++ universidade/Componentes/cadastroAluno.js
@@ -0,0 +1,114 @@
+import React, {useState} from 'react'
+import {View, Text, FlatList, TextInput, Button, DrawerLayoutAndroidBase} from 'react-native'
+
+export default function App(props){
+
+  const [nome, setNome] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [email, setEmail] = useState("");
+
+  // cria um vetor vazio
+  const [vetor, setVetor] = useState([]);
+
+  const renderiza = ({item})=>{
+    return(
+      <View style={{flexDirection:'row', backgroundColor:'red'}}>
+        <Text style={{flex:1, color:'white'}}> {item.id} </Text> 
+        <Text style={{flex:1, color:'white'}}> {item.nome} </Text>
+        <Text style={{flex:1, color:'white', justifyContent:'flex-end'}}> {item.cidade} </Text>
+      </View>
+    );
+  }
+
+  const separador = ()=>{
+    return( 
+        <View style={{height:3, backgroundColor:'black', width:'100%'}}></View>
+     );
+  }
+
+  const cabecalho = ()=>{
+    return( 
+      <View style={{height:20, backgroundColor:'black', width:'100%', marginTop:20}}>
+            <Text style={{color:'white', textAlign:'center'}}>Dados que foram cadastrados</Text>
+        </View>
+     );
+  }
+
+  const rodape = ()=>{
+    return( 
+        <View style={{height:10, backgroundColor:'black', width:'100%'}}>
+        </View>
+     );
+  }
+
+  return(
+    <View>
+
+      <View style={{flex:1}}>
+        <TextInput
+         value={nome}
+         placeholder="Digite seu nome!" 
+         style={{borderWidth:2, borderColor:"red"}}
+         onChangeText={(text) => {setNome(text)}}
+        />
+
+      <TextInput
+         value={endereco}
+         placeholder="Digite seu endereço!" 
+         style={{borderWidth:2, borderColor:"red"}}
+         onChangeText={(text) => {setEndereco(text)}}
+        />
+
+        <TextInput
+         value={email}
+         placeholder="Digite seu email!" 
+         style={{borderWidth:2, borderColor:"red"}}
+         onChangeText={(text) => {setEmail(text)}}
+        />
+
+        <TextInput
+         value={telefone}
+         placeholder="Digite seu telefone!" 
+         style={{borderWidth:2, borderColor:"red", marginBottom:20}}
+         onChangeText={(text) => {setTelefone(text)}}
+        />
+
+        <Button 
+          title="Cadastrar"
+          color='#bd0000'
+          onPress={
+            ()=>{
+              const auxVex = {id:vetor.length+1,nome: nome, cidade: cidade}
+
+              // adiciona os dados no vetor
+              vetor.push(auxVex)
+
+              // cria uma cópia do vetor para que o flatlista entenda que houve uma alteracao no vetor inicial
+              //const copiaVetor = vetor.slice()
+
+              // outra maneira de cria uma cópia
+              const copiaVetor = [...vetor]
+
+              // salva a copia novamente dentro da variavel vetor
+              setVetor(copiaVetor);
+
+              //setVetor([...vetor,auxVex]);
+              console.log(vetor)
+            }
+          }
+        />
+
+      <FlatList 
+          data ={vetor} 
+          keyExtractor={(item)=>{item.chave}}
+          renderItem={renderiza}
+          ItemSeparatorComponent={separador}
+          ListHeaderComponent={cabecalho}
+          ListFooterComponent={rodape}
+      />
+    </View>
+    </View>
+  );
+}
