036fd3ce679d6ebee6a443136ccfd64c2746ea62
Felipe Machado <Felipe.Machado@fit-tecnologia.org.br>
2022-11-01 00:26:11 -0300

    Finalizada tela histórico aluno

--- universidade/API/VisualizaTurma/visualizaHistoricoBanco.js
+++ universidade/API/VisualizaTurma/visualizaHistoricoBanco.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, Dimensions } from 'react-native'
+import { View, Text, FlatList, Dimensions, Image } from 'react-native'
 import { collection, getDocs } from 'firebase/firestore'
 
 import db from '../../config'
@@ -7,7 +7,6 @@ import db from '../../config'
 export default function VisualizaHistoricoBanco({ route }) {
     const colecaoHistorico = collection(db, 'Histórico');
     const colecaoAluno = collection(db, 'Aluno');
-
     const [vetorHistorico, setVetorHistorico] = useState([]);
     const [dadosTela, setDadosTela] = useState([]);
     const [vetorAluno, setVetorAluno] = useState([]);
@@ -37,7 +36,7 @@ export default function VisualizaHistoricoBanco({ route }) {
                 const snapshot = await getDocs(colecaoAluno)
 
                 for (let i = 0; i < snapshot.docs.length; i++) {
-                    const dadoAluno = { id: snapshot.docs[i].id, nome: snapshot.docs[i].data().nome, cidade: snapshot.docs[i].data().cidade, endereco: snapshot.docs[i].data().endereco }
+                    const dadoAluno = { id: snapshot.docs[i].id, nome: snapshot.docs[i].data().nome, cidade: snapshot.docs[i].data().cidade, endereco: snapshot.docs[i].data().endereco, foto:snapshot.docs[i].data().foto }
                     vetorAluno.push(dadoAluno)
                 }
 
@@ -54,7 +53,7 @@ export default function VisualizaHistoricoBanco({ route }) {
                                 console.log('entrouIF2')
                                 console.log('Nome: ', vetorAluno)
                                 console.log('Nota: ', vetorHistorico)
-                                dadosTela.push({ id:item.id, nome: item.nome, frequencia: vetorHistorico[i].frequencia, nota: vetorHistorico[i].nota })
+                                dadosTela.push({ id:item.id, nome: item.nome, foto:item.foto, frequencia: vetorHistorico[i].frequencia, nota: vetorHistorico[i].nota})
                             }
                         })
                     }
@@ -72,34 +71,44 @@ export default function VisualizaHistoricoBanco({ route }) {
             console.log(erro.message)
         }
 
-        const separador = () => {
-            return (
-                <View style={{ height: 1, backgroundColor: 'black', width: '100%' }}></View>
-            );
-        }
+    }
 
-        const renderiza = ({ item }) => {
-            const tamanho = Dimensions.get('window').height / matriculas.length
-            return (
-                <View style={{ flex: 1, flexDirection: 'row'}}>
-                    <Text>{item.nome}</Text>
-                    <Text>{item.frequencia}</Text>
-                    <Text>{item.nota}</Text>
-                </View>
-            );
-        }
-
+    const separador = () => {
         return (
-            <View style={{ flex: 1 }}>
-                <FlatList
-                    data={dadosTela}
-                    keyExtractor={item => item.id}
-                    renderItem={renderiza}
-                    ItemSeparatorComponent={separador}
-                    ListHeaderComponent={separador}
-                    ListFooterComponent={separador}
-                /> 
+            <View style={{ height: 1, backgroundColor: 'black', width: '100%' }}></View>
+        );
+    }
+
+    const renderiza = ({ item }) => {
+        
+        return (
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent:'space-between'}}>
+                <Text>{item.nome}</Text>
+                <Text>{item.frequencia}</Text>
+                <Text>{item.nota}</Text>
             </View>
         );
     }
+
+    return (
+        <View style={{ flex: 1 }}>
+            <Image
+                    style={{ width: 300, height: 300, resizeMode: 'contain' }}
+                    source={{ uri: ()=>{retornaFoto()}}}
+            />
+            <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+                <Text>Aluno</Text>
+                <Text>Frequencia</Text>
+                <Text>Nota</Text>
+            </View>
+            <FlatList
+                data={dadosTela}
+                keyExtractor={item => item.id}
+                renderItem={renderiza}
+                ItemSeparatorComponent={separador}
+                ListHeaderComponent={separador}
+                ListFooterComponent={separador}
+            /> 
+        </View>
+    );
 }
