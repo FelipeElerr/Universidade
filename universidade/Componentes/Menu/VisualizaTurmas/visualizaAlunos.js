cbe95930c4bd4716ee3347281717ee542fae335d
Felipe Eler <200738@facens.br>
2022-11-13 20:27:48 -0300

    Loading alunos

--- universidade/Componentes/Menu/VisualizaTurmas/visualizaAlunos.js
+++ universidade/Componentes/Menu/VisualizaTurmas/visualizaAlunos.js
@@ -1,18 +1,51 @@
-import React from 'react'
-import { View, Text, ScrollView } from 'react-native'
-import VisualizaAlunosBanco from '../../../API/VisualizaTurma/visualizaAlunosBanco'
+import React from 'react';
+import { View, ActivityIndicator, Button, Text} from 'react-native';
+import VisualizaAlunosBanco from '../../../API/VisualizaTurma/visualizaAlunosBanco';
+import {useState} from "react";
 
 import PlanoDeFundo from '../../Configuracoes/planoDeFundo'
 
-export default function VisualizaAlunos({route}){
-    return(
-        <PlanoDeFundo>
-            <View style={{marginLeft:'300', marginRight:'300'}}>
-            
-                <VisualizaAlunosBanco disciplina={route.params.paramKey} navigation={route.params.navigation}/>
-               
+export default function VisualizaAlunos({ route }) {
+    const [loading, setLoading] = useState(true)
+
+    setTimeout(() => {
+        setLoading(false)
+    }, 10000)
+
+    if (loading == true) {
+        return (
+            <View>
+                <PlanoDeFundo>
+                <ActivityIndicator size="large" color="blue" animating={loading} />
+                </PlanoDeFundo>
                 
             </View>
-        </PlanoDeFundo>
-    )
+        )
+    }
+
+    if (loading == false) {
+        try {
+            return (
+                <PlanoDeFundo>
+                    <View style={{ marginLeft: '300', marginRight: '300' }}>
+                    <VisualizaAlunosBanco disciplina={route.params.paramKey} navigation={route.params.navigation} />
+                </View>
+                </PlanoDeFundo>
+                
+            )
+        } catch (error) {
+            return (
+                <PlanoDeFundo>
+                    <View>
+                        <Text style={{ textAlign: 'center', fontSize: 20, marginTop: 15 }}>
+                            Erro ao carregar informações!
+                        </Text>
+
+                        <Button title='Tente novamente' onPress={() => { setLoading(true) }} />
+                    </View>
+                </PlanoDeFundo>
+            )
+        }
+
+    }
 }
